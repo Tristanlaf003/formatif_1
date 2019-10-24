51d0185734ee26029afbb7ac1782d7e190e8895b
Tristanlaf003 <55155457+Tristanlaf003@users.noreply.github.com>
2019-10-24 12:31:32 -0400

    test2

---
+++ Documents/GitHub/Tristanlaf003/DesignWeb/Exercice jQuery1 StormTrooper/Bootstrap/js/monscripts.js
@@ -0,0 +1,12 @@
+$('#stormBlanc').attr("title","Pas facile la vie de Stormtrooper");
+$('#stormBlanc').attr("alt","image d'un stormtrooper blanc");
+$('#stormBlanc').attr("src","images/nouveauStorm.jpeg");
+$('#fontAChanger').attr("class", "fas fa-fighter-jet");
+$('.row4 h1').css("font-size","4rem");
+$('.orange').css("opacity", "0.8");
+$('.fondNoir').css("background-color","gray");
+$('.fondBlanc').css("background-color","lightgray");
+$('#titre').html("<h1>Vive les Stormtroopers<\h1>");
+$('#PereDeLuke').html("Père de Luke Skywalker");
+$('#cache').hide();
+
---
+++ Documents/GitHub/Tristanlaf003/DesignWeb/Jquery/Bootstrap/js/monscripts.js
@@ -0,0 +1,10 @@
+$('.bouton1').click(function(){
+    $('.p1').toggle();
+});
+$('.bouton2').click(function(){
+    $('.p2').toggle();
+});
+$('.bouton3').click(function(){
+    $('.p3').toggle();
+});
+
